b26d02b826a62b86fa3ad573c4d0292b2bfaa904
Daniel Montesclaros <147793265+constantine2003@users.noreply.github.com>
2025-04-09 19:14:03 +0800

    PROFILE INFO SKELETON BUILT BITCH

---
+++ src/app/(routes)/porfolio_creation/page.jsx
@@ -0,0 +1,64 @@
+import styles from './porfoliocreation.module.css';
+
+function PortfolioCreationPage() {
+  return (
+    <div className={styles.container}>
+
+      <div className={styles.profileinfo}>
+
+        {/* Profile DIV */}
+        <div className={styles.profilediv}>
+
+        </div>
+        {/* END of Profile DIV */}
+
+        {/* CONTACT INFO  DIV */}
+        <div className={styles.infodiv}>
+
+        </div>
+        
+        <div className={styles.contactinfo}>
+
+        </div>
+        {/* END OF CONTACT INFO DIV */}
+
+        {/* ADDRESS INFO DIV */}
+        <div className={styles.infodiv}>
+
+        </div>
+        <div className={styles.addressinfo}>
+
+        </div>
+        {/* END OF ADDRESS INFO DIV */}
+
+
+        {/* WORK INFO TEXT DIV */}
+        <div className={styles.infodiv}>
+
+        </div>
+        <div className={styles.workinfo}>
+
+        </div>
+        {/* END WORK INFO DIV */}
+
+        {/* SKILLS INFO DIV */}
+        <div className={styles.infodiv}>
+
+        </div>
+        <div className={styles.skillsinfo}>
+
+        </div>
+        {/* END OF SKILLS INFO  DIV */}
+
+
+      </div>
+
+      <div className={styles.porfoliodescription}>
+
+      </div>
+
+    </div>
+  );
+}
+
+export default PortfolioCreationPage;
