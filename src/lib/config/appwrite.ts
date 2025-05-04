4066e21760046bbf2679d652d06ba4d02e56ae88
laurenx <santillanmitchlauren@gmail.com>
2025-05-04 12:48:37 +0800

    Update

--- src/lib/config/appwrite.ts
+++ src/lib/config/appwrite.ts
@@ -1,13 +1,12 @@
 // lib/config/appwrite.ts
-import { Client, Account, Databases, Storage } from "appwrite";
+import { Client, Databases, Storage } from "appwrite";
 
 const client = new Client();
 client
   .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT as string)
   .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID as string);
 
-const account = new Account(client);
 const databases = new Databases(client);
 const storage = new Storage(client);
 
-export { client, account, databases, storage };
+export { client, databases, storage };
