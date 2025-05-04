f590ff2bfc7bba750bbb484d24bef0073f9c6cff
laurenx <santillanmitchlauren@gmail.com>
2025-05-04 13:07:27 +0800

    Revert "Update"
    This reverts commit 4066e21760046bbf2679d652d06ba4d02e56ae88.

--- src/lib/config/appwrite.ts
+++ src/lib/config/appwrite.ts
@@ -1,12 +1,13 @@
 // lib/config/appwrite.ts
-import { Client, Databases, Storage } from "appwrite";
+import { Client, Account, Databases, Storage } from "appwrite";
 
 const client = new Client();
 client
   .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT as string)
   .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID as string);
 
+const account = new Account(client);
 const databases = new Databases(client);
 const storage = new Storage(client);
 
-export { client, databases, storage };
+export { client, account, databases, storage };
