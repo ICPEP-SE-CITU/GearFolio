c3b0a7b90a241d9a20725f05b72b9383f3c7060a
M4monski <162318730+M4monski@users.noreply.github.com>
2025-05-07 00:46:46 +0800

    (Refactor): Post CSS Version,

--- src/components/features/aiRecomm/aiRecommButton.js
+++ src/components/features/aiRecomm/aiRecommButton.js
@@ -15,7 +15,7 @@ export default function AiRecommButton() {
   return (
     <button
       onClick={handleClick}
-      className="hover:scale-115 hover:rounded-3xl transition duration-300 hover:cursor-pointer"
+      className="hover:scale-115 hover:rounded-3xl transition duration-300 hover:cursor-pointer z-50"
     >
       <Image
         src="/aiRecommButton.svg"
