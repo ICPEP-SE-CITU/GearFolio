4156dab0393c2af08e1819dae150352506f39583
Nana-Boi <bananay24@gmail.com>
2025-05-07 15:17:03 +0800

    fixed footer? and commented out "export metadata" in app/layout. was showing errors.

--- src/app/(routes)/help-support/page.js
+++ src/app/(routes)/help-support/page.js
@@ -1,7 +1,8 @@
 'use client';
 
 import { useState } from 'react';
-import footer from '../../layout/Footer';
+import footer from '../../layout';
+import header from '../../layout';
 
 function FAQItem({ question, answer, activeIndex, index, setActiveIndex }) {
   const isOpen = activeIndex === index;
